c48c597873f507b06d35e9976529844703660087
ravikatha <ravikiran@kaatha.com>
2015-02-04 16:26:27 +0530

    Updated Copyright Year & Version

--- DC.Crayon.Wlw/Properties/AssemblyInfo.cs
+++ DC.Crayon.Wlw/Properties/AssemblyInfo.cs
@@ -9,15 +9,15 @@ using System.Security;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("DotCastle™")]
 [assembly: AssemblyProduct("DotCastle™ Crayon Syntax Snippet")]
-[assembly: AssemblyCopyright("Copyright © 2013 - 2014, DotCastle™ TechnoSolutions Private Limited, INDIA. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2013 - 2015, DotCastle™ TechnoSolutions Private Limited, INDIA. All rights reserved.")]
 [assembly: AssemblyTrademark("DotCastle™ is a trademark of DotCastle™ TechnoSolutions Private Limited, INDIA")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en", UltimateResourceFallbackLocation.MainAssembly)]
 [assembly: CLSCompliant(false)]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
 
 [assembly: Guid("9B4FC5D9-9B7E-42B4-8A50-DA76123E1DB9")]
 [assembly: AllowPartiallyTrustedCallers]
